42ad63527035f0a078a4f22d8cdaafc2eccf787c
Mayuri Harkulkar Intertek <mayuri.harkulkar@intertek.com>
2023-11-17 02:22:52 -0600

    BA 17- Intial checking

---
+++ SMART_DataMaskingScheduler/DataMasking.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Configuration;
+//using System.Transactions;
+using System.IO;
+using System.Data.SqlClient;
+using System.Data;
+using System.Net.Http;
+using System.Threading;
+using System.Globalization;
+using System.Diagnostics;
+//using log4net;
+
+namespace SMART_DataMaskingScheduler
+{
+    class DataMasking
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
